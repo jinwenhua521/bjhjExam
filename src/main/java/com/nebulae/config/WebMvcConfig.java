02804d99cdf4af56d6e43035a8c185e835cf4555
Lazzaro <734701641@qq.com>
2020-05-07 09:44:50 +0800

    删除冗余代码

--- src/main/java/com/nebulae/config/WebMvcConfig.java
+++ src/main/java/com/nebulae/config/WebMvcConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-import com.nebulae.license.LicenseCheckInterceptor;
 import com.nebulae.utils.AESUtil;
 
 
